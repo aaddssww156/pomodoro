5cafa781b6d3b498cfc8dde6300d5251b3a92377
aaddssww156 <lemnik25@gmail.com>
2023-11-13 13:42:29 +1000

    Готовые toasts для windows

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "log"
+import (
+	"log"
+	"time"
+
+	"github.com/go-toast/toast"
+)
 
 func main() {
-	log.Println("Hello world!")
+	notificationWork := toast.Notification{
+		AppID:   "Pomodoro",
+		Title:   "Work",
+		Message: "Started 25 min work!",
+	}
+
+	notificationRest := toast.Notification{
+		AppID:   "Pomodoro",
+		Title:   "Rest",
+		Message: "Have a rest for 5 minutes!",
+	}
+
+	for {
+		err := notificationWork.Push()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		time.Sleep(25 * time.Minute)
+
+		err = notificationRest.Push()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		time.Sleep(5 * time.Minute)
+	}
 }
